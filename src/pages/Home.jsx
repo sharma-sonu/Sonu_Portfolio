ed24a16d6fd28ebd02a7234b2c1e135ab67b431a
sonu sharma <sonuhyt99058@gmail.com>
2025-11-03 09:05:39 +0530

    Added vercel.json configuration for routing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "../components/HeroSection";
 import About from "../components/About";
 import Projects from "../components/Projects";
+import Certifications from "../pages/Certifications"
 
 
 const Home = () => {
   return (
     <>
+    {/* <About/> */}
       <HeroSection />
        <Projects />
        <Certifications />
